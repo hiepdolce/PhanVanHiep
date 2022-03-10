32723c38967c55c3da32ab6f042d09fe3105fe00
hiepdolce <101303358+hiepdolce@users.noreply.github.com>
2022-03-10 09:08:17 +0700

    Delete Bai1.4.js

--- Bai1.4.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("demo1").innerHTML="Tài liệu học CSS";
-document.getElementById("demo2").innerHTML="Tài liệu học MySQL";
-document.getElementById("demo3").innerHTML="Tài liệu học PHP";
